974785de277157b7214fb80b80759d5ac51e1277
Orlando Javier <oj.flores118@gmail.com>
2024-04-11 20:51:05 -0600

    Login para sistem

---
+++ main.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    const window = new BrowserWindow({
+        width: 820,
+        height:  600,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        show: true
+    })
+
+    window.loadFile('index.html')
+}
+
+app.whenReady().then(() => { createWindow() } )
